7297d55b3a184638cc746fd3dfc37c34221cb4be
Ryne <ryneschillinger1@AA99.local>
2023-01-09 16:40:04 -0800

    added npm version

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 4200, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
