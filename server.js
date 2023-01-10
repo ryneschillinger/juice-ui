b891a34e0220be444604156484b1421ef4beb425
Ryne <ryneschillinger1@AA99.local>
2023-01-09 16:58:36 -0800

    added npm version

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.set('port', process.env.PORT || 3000);
-
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(app.get('port'), () => console.log(`Node server listening on port ${app.get('port')}!`));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
