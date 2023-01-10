c657824c7de0816e22e00d43f5bf25841e002317
Ryne <ryneschillinger1@AA99.local>
2023-01-09 17:16:44 -0800

    fixed scripts

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/juice-ui'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/juice-ui/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function(){
